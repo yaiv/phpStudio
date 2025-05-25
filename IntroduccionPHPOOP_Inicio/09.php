34bb918a0d77f5b9026f92f4e41fcde189b9aa57
yaiv <yaiv.gm@gmail.com>
2025-05-25 11:01:23 -0600

    Temas: BD y POO con MySQL y PDO

--- IntroduccionPHPOOP_Inicio/09.php
+++ IntroduccionPHPOOP_Inicio/09.php
@@ -1,6 +1,24 @@
 <?php include 'includes/header.php';
 
-//Conectar a la BD con Mysqli
+//Conectar a la BD con Mysqli y mysqli suporta funciones 
+//Mejor performance y sin inyeccion de sql
 
+$db = new mysqli('localhost', 'root', 'root', 'powermode'); //conexion a bd
 
+//   var_dump($db);  este opjeto ya tiene todos los metodos 
+$query = "SELECT titulo, precio FROM ropa";     //la consulta que se va a realizar 
+
+$stmt = $db->prepare($query);  //que se prepare para realizar la consulta 
+
+$stmt->execute(); //ejecutamos el stmt
+
+$stmt->bind_result($titulo, $precio); //Crea automaticamente una variable con los rsultados de la consulta
+//Solo trae un resultado 
+$stmt->fetch(); //Se trae la variable con la informacion
+
+//para traer todos los resultados, se ingresa en un while 
+
+while($stmt->fetch()):
+    var_dump($titulo, $precio); //Se imprime el resultado 
+endwhile;
 include 'includes/footer.php';
--- IntroduccionPHPOOP_Inicio/10.php
+++ IntroduccionPHPOOP_Inicio/10.php
@@ -1,4 +1,32 @@
 <?php include 'includes/header.php';
 
+//Conectar a la BD con PDO 
+//Mayor soporte a a diferentes BD 
 
+$db = new PDO('mysql:host=localhost; dbname=powermode', 'root', 'root');
+
+$query = "SELECT titulo, precio FROM ropa";  
+
+//preparar sentencia o consulta
+$stmt = $db->prepare($query); 
+
+//se ejecuta 
+$stmt->execute();
+
+//Se obtienen los resultados con el forch 
+$resultado = $stmt->fetchAll( PDO::FETCH_ASSOC ); //Se especifica como se desean los datos 
+
+
+//Se imprime 
+
+foreach($resultado as $ropa):
+echo $ropa['titulo'];
+echo "<br>";
+echo $ropa['precio'];
+echo "<br>";
+endforeach;
+//Se imprime 
+// echo "<pre>";
+// var_dump($resultado);
+// echo "</pre>";
 include 'includes/footer.php';
